4cf1e580be4aab938e6758ff97d85ac72041578e
Alexander Jung <a.jung@lancs.ac.uk>
2020-12-22 16:56:37 +0100

    run/run: Use parsing to determine runtime
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

---
+++ run/run.go
@@ -0,0 +1,61 @@
+package run
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Authors: Alexander Jung <a.jung@lancs.ac.uk>
+//
+// Copyright (c) 2020, Lancaster University.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import (
+  "fmt"
+)
+
+type RunnerType int
+
+const (
+  UNKNOWN RunnerType = iota
+  EMPTY
+  RUNC
+)
+
+// ParseRunImage returns the name of the 
+func ParseRunImage(image string) (RunnerType, error) {
+  if len(image) == 0 {
+    return EMPTY, fmt.Errorf("Image definition empty")
+  }
+
+  ref, err := Parse(image)
+  if err != nil {
+    return UNKNOWN, err
+  }
+
+  if len(ref.Runtime) == 0 {
+    ref.Runtime = DefaultRuntime
+  }
+
+  return UNKNOWN, nil
+}
