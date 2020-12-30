939d30427ad6e3f4f956bfe5df86eb6b7916be80
Alexander Jung <a.jung@lancs.ac.uk>
2020-12-30 07:49:37 +0000

    run/runc: Import mount options parser
    This commit pulls in an unexported function from runc's
    libcontainer[0] which parses string representation of
    Unix mounting options.
    
    [0]: https://github.com/opencontainers/runc/blob/v1.0.0-rc92/libcontainer/specconv/spec_linux.go#L727-L813
    
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- run/runc.go
+++ run/runc.go
@@ -331,3 +331,91 @@ func (r *RuncRunner) Destroy() error {
   }
   return nil
 }
+
+// parseMountOptions parses the string and returns the flags, propagation
+// flags and any mount data that it contains.
+func parseMountOptions(options []string) (int, []int, string, int) {
+  var (
+    flag     int
+    pgflag   []int
+    data     []string
+    extFlags int
+  )
+  flags := map[string]struct {
+    clear bool
+    flag  int
+  }{
+    "acl":           {false, unix.MS_POSIXACL},
+    "async":         {true, unix.MS_SYNCHRONOUS},
+    "atime":         {true, unix.MS_NOATIME},
+    "bind":          {false, unix.MS_BIND},
+    "defaults":      {false, 0},
+    "dev":           {true, unix.MS_NODEV},
+    "diratime":      {true, unix.MS_NODIRATIME},
+    "dirsync":       {false, unix.MS_DIRSYNC},
+    "exec":          {true, unix.MS_NOEXEC},
+    "iversion":      {false, unix.MS_I_VERSION},
+    "lazytime":      {false, unix.MS_LAZYTIME},
+    "loud":          {true, unix.MS_SILENT},
+    "mand":          {false, unix.MS_MANDLOCK},
+    "noacl":         {true, unix.MS_POSIXACL},
+    "noatime":       {false, unix.MS_NOATIME},
+    "nodev":         {false, unix.MS_NODEV},
+    "nodiratime":    {false, unix.MS_NODIRATIME},
+    "noexec":        {false, unix.MS_NOEXEC},
+    "noiversion":    {true, unix.MS_I_VERSION},
+    "nolazytime":    {true, unix.MS_LAZYTIME},
+    "nomand":        {true, unix.MS_MANDLOCK},
+    "norelatime":    {true, unix.MS_RELATIME},
+    "nostrictatime": {true, unix.MS_STRICTATIME},
+    "nosuid":        {false, unix.MS_NOSUID},
+    "rbind":         {false, unix.MS_BIND | unix.MS_REC},
+    "relatime":      {false, unix.MS_RELATIME},
+    "remount":       {false, unix.MS_REMOUNT},
+    "ro":            {false, unix.MS_RDONLY},
+    "rw":            {true, unix.MS_RDONLY},
+    "silent":        {false, unix.MS_SILENT},
+    "strictatime":   {false, unix.MS_STRICTATIME},
+    "suid":          {true, unix.MS_NOSUID},
+    "sync":          {false, unix.MS_SYNCHRONOUS},
+  }
+  propagationFlags := map[string]int{
+    "private":     unix.MS_PRIVATE,
+    "shared":      unix.MS_SHARED,
+    "slave":       unix.MS_SLAVE,
+    "unbindable":  unix.MS_UNBINDABLE,
+    "rprivate":    unix.MS_PRIVATE | unix.MS_REC,
+    "rshared":     unix.MS_SHARED | unix.MS_REC,
+    "rslave":      unix.MS_SLAVE | unix.MS_REC,
+    "runbindable": unix.MS_UNBINDABLE | unix.MS_REC,
+  }
+  extensionFlags := map[string]struct {
+    clear bool
+    flag  int
+  }{
+    "tmpcopyup": {false, configs.EXT_COPYUP},
+  }
+  for _, o := range options {
+    // If the option does not exist in the flags table or the flag
+    // is not supported on the platform,
+    // then it is a data value for a specific fs type
+    if f, exists := flags[o]; exists && f.flag != 0 {
+      if f.clear {
+        flag &= ^f.flag
+      } else {
+        flag |= f.flag
+      }
+    } else if f, exists := propagationFlags[o]; exists && f != 0 {
+      pgflag = append(pgflag, f)
+    } else if f, exists := extensionFlags[o]; exists && f.flag != 0 {
+      if f.clear {
+        extFlags &= ^f.flag
+      } else {
+        extFlags |= f.flag
+      }
+    } else {
+      data = append(data, o)
+    }
+  }
+  return flag, pgflag, strings.Join(data, ","), extFlags
+}
