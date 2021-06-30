6bc37507e42c1dcb8b68fdd77c80ac72d5369eb7
Alexander Jung <a.jung@lancs.ac.uk>
2021-06-30 13:08:43 +0200

    spec: Provide more options
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- spec/runtime.go
+++ spec/runtime.go
@@ -30,15 +30,48 @@ package spec
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+import (
+  "time"
+)
+
 type RuntimeSpec struct {
-  CpuSets       string  `short:"s" long:"cpu-sets" yaml:"cpu_sets" description:"Specify which CPUs to run experiments on."`
-  DryRun        bool    `short:"D" long:"dry-run" yaml:"dry_run" description:"Run without affecting the host or running the jobs."`
-  ScheduleGrace int     `short:"g" long:"scheduler-grace" yaml:"schedule_grace" description:"Number of seconds to gracefully wait in the scheduler." default:"1"`
-  WorkDir       string  `short:"w" long:"workdir" yaml:"workdir" description:"Specify working directory for outputting results, data, file systems, etc."`
-  AllowOverride bool    `short:"O" long:"override" yaml:"allow_io_override" description:"Override contents in directories (otherwise tasks allowed to fail)."`
-  HostNetwork   string  `short:"h" long:"host-net" yaml:"host_network" description:"The host network to attach the bridge to." default:"eth0"`
-  BridgeName    string  `short:"b" long:"bridge" yaml:"bridge_name" description:"The bridge network to attach services to." default:"ukbench0"`
-  BridgeSubnet  string  `short:"n" long:"subnet" yaml:"bridge_subnet" description:"The subnet of addresses for container services." default:"172.88.0.1/16"`
-  MaxRetries    int     `short:"r" long:"max-retries" yaml:"max_retries" description:"Maximum number of retries for a failed permutation" default:"0"`
-  Scheduler     string  `short:"x" long:"scheduler" yaml:"scheduler" description:"Parameter permutation scheduler" default:"simple"`
+  DryRun            bool          `short:"D" long:"dry-run"          yaml:"dry_run"          description:"Run without affecting the host or running the jobs."`
+  AllowOverride     bool          `short:"O" long:"override"         yaml:"override"         description:"Override contents in directories (otherwise tasks allowed to fail)."`
+
+  // Information output configuration
+  Output            string        `short:"o" long:"output"           yaml:"output"           description:"The output channel to send printer output (valid output: stdout, file, tcp, udp)." default:"stdout"`
+  Target            string        `          long:"target"           yaml:"target"           description:"For output 'file' the location, for 'tcp' or 'udp' the url (host:port) to the server."`
+
+  // System host configuration
+  CpuSets           string        `short:"s" long:"cpu-sets"         yaml:"cpu_sets"         description:"Specify which CPUs to run experiments on."`
+  ProcFS            string        `          long:"procfs"           yaml:"procfs"           description:"Path to procfs." default:"/procfs"`
+  WorkDir           string        `short:"w" long:"workdir"          yaml:"workdir"          description:"Specify working directory for outputting results, data, file systems, etc."`
+  CacheDir          string        `          long:"cachedir"         yaml:"cachedir"         description:"Specify cache directory for temporary files, etc."`
+  ResultsDir        string        `          long:"resultsdir"       yaml:"resultsdir"       description:"Specify directory for output files of results, etc."`
+  LibvirtURI        string        `short:"l" long:"libvirt"          yaml:"libvirt"          description:"Connection URI to libvirt daemon." default:"qemu:///system"`
+  ContainerdURI     string        `short:"c" long:"containerd"       yaml:"containerd"       description:"Connection URI to containerd's GRPC server." default:"/run/containerd/containerd.sock"`
+
+  // Network configuration
+  HostNetwork       string        `short:"h" long:"host-net"         yaml:"host_network"     description:"The host network to attach the bridge to." default:"eth0"`
+  BridgeName        string        `short:"b" long:"bridge"           yaml:"bridge_name"      description:"The bridge network to attach services to." default:"ukbench0"`
+  BridgeSubnet      string        `short:"n" long:"subnet"           yaml:"bridge_subnet"    description:"The subnet of addresses for container services." default:"172.88.0.1/16"`
+
+  // Scheduler configuration      
+  Scheduler         string        `short:"x" long:"scheduler"        yaml:"scheduler"        description:"Parameter permutation scheduler." default:"simple"`
+  ScheduleGrace     int           `short:"g" long:"scheduler-grace"  yaml:"schedule_grace"   description:"Number of seconds to gracefully wait in the scheduler." default:"1"`
+  MaxRetries        int           `short:"r" long:"max-retries"      yaml:"max_retries"      description:"Maximum number of retries for a failed permutation." default:"0"`
+
+  // Metrics and measurement configuration
+  MeasureCPU        bool          `          long:"measure-cpu"      yaml:"measure_cpu"      description:"Enable cpu metrics."`
+  MeasureMEM        bool          `          long:"measure-mem"      yaml:"measure_mem"      description:"Enable memory metrics."`
+  MeasureDISK       bool          `          long:"measure-disk"     yaml:"measure_disk"     description:"Enable disk metrics."`
+  MeasureNET        bool          `          long:"measure-net"      yaml:"measure_net"      description:"Enable network metrics."`
+  MeasureIO         bool          `          long:"measure-io"       yaml:"measure_io"       description:"Enable I/O metrics."`
+  MeasurePressure   bool          `          long:"measure-pressure" yaml:"measure_pressure" description:"Enable pressure metrics (requires kernel 4.20+)."`
+  MeasureHost       bool          `          long:"measure-host"     yaml:"measure_host"     description:"Enable host metrics."`
+  MeasureAllMetrics bool          `short:"A" long:"measure-all"      yaml:"measure_all"      description:"Enable all metrics."`
+  Frequency         time.Duration `short:"f" long:"frequency"        yaml:"frequency"        description:"Frequency (in seconds) for collecting metrics." default:"1"`
+
+  NetworkDevice     string        `          long:"netdev"           yaml:"netdev"           description:"The network device used for the virtual traffic"`
+	StorageDevice     string        `          long:"storedev"         yaml:"storedev"         description:"The storage device used for the virtual block devices"`
 }
